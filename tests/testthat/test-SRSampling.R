9c90572bb58a2d8b6030759b3bfb605be670d443
Qian Ye <yexipaopao@gmail.com>
2020-11-12 15:36:04 -0800

    update test

--- tests/testthat/test-SRSampling.R
+++ tests/testthat/test-SRSampling.R
@@ -1,11 +1,14 @@
+# report error when sample size > population size
 test_that("exceeded size test", expect_error(SRSampling(x=rnorm(10), size=11), "size must be no greater than the length of x"))
 
+# report error when length(x)=1
 test_that("length of (x)= 1 test",expect_error(SRSampling(x=rnorm(1), size=1), "x must be a numeric vector with length >1"))
 
+# test the length of output
 rep <- 20
 test_that("length of sample means", expect_length(SRSampling(x=rnorm(100), size=10, rep=20)$sample.mean$value, rep))
 
-
+# test the named list output
 test_that("names of the output list with show.SRS=T", expect_named(SRSampling(x=rnorm(100), size=10, rep=20, show.SRS=TRUE),c("sample.mean","sample.se", "SRS")))
 
 test_that("names of the output list", expect_named(SRSampling(x=rnorm(100), size=10, rep=20),c("sample.mean","sample.se")))
