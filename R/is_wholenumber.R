dd88718ae05c883f9c039bffe6adf2c61c18b401
Qian Ye <yexipaopao@gmail.com>
2020-11-12 18:06:46 -0800

    add tests for is_wholenumber

--- R/is_wholenumber.R
+++ R/is_wholenumber.R
@@ -1,2 +1,16 @@
+#' is_wholenumber: check if a number is an integer
+#'
+#' @param x a numeric vector
+#' @param tol
+#'
+#' @return logical indicating whether x is integer
+#'
+#'
+#' @examples
+#' is_wholenumber(1.5)
+#' is_wholenumber(1:5)
+#' is_wholenumber(c(1,2.5,NA))
 is_wholenumber <-
-  function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
+  function(x, tol = .Machine$double.eps^0.5) {
+    abs(x - round(x)) < tol
+  }
