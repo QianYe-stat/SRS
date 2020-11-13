6a6a22f8f42b0524fd111e54c83b4595826d4029
Qian Ye <yexipaopao@gmail.com>
2020-11-12 18:30:10 -0800

    test is_wholenumber

--- tests/testthat/test-is_wholenumber.R
+++ tests/testthat/test-is_wholenumber.R
@@ -1,3 +1,5 @@
 test_that("Is it a integer", {
   expect_equal(is_wholenumber(2), TRUE)
+  expect_equal(is_wholenumber(2.5), FALSE)
+  expect_equal(is_wholenumber(c(1,2.5,3)), c(TRUE,FALSE,TRUE))
 })
